4f20777b5d4bc9b8c07ab049a954e8fd27361e03
jeerapat <jeerapat@mfec.co.th>
2022-12-30 21:31:07 +0700

    set default gin

--- server.go
+++ server.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	fmt.Println("Please use server.go for main file")
 	fmt.Printf("start at port: %q\n", os.Getenv("PORT"))
 	fmt.Printf("DB URL: %q\n", os.Getenv("DATABASE_URL"))
+
+	r := gin.Default()
+	r.SetTrustedProxies([]string{"127.0.0.1"})
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run(":" + os.Getenv("PORT"))
 }
