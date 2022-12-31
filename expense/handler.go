88c793dda01e2fc5062b800c4e0b3dfba299a139
jeerapat <jeerapat@mfec.co.th>
2022-12-31 10:17:23 +0700

    init create  expense handler

---
+++ expense/handler.go
@@ -0,0 +1,23 @@
+package expense
+
+import (
+	"database/sql"
+	"errors"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type handler struct {
+	DB *sql.DB
+}
+
+func NewHandler(db *sql.DB) *handler {
+	return &handler{
+		DB: db,
+	}
+}
+
+func (h *handler) CreateExpense(c *gin.Context) error {
+	return c.AbortWithError(http.StatusInternalServerError, errors.New("not implemented yet"))
+}
