9e5e8965b82eedd9ed67dbbccc10b8e69e83be28
jeerapat <jeerapat@mfec.co.th>
2023-01-01 07:19:23 +0700

    add unit test for get detail

--- expense/handler_test.go
+++ expense/handler_test.go
@@ -70,9 +70,30 @@ func TestCreateExpense(t *testing.T) {
 
 func TestGetExpenseDetailById(t *testing.T) {
 	// Arrange
+	req := httptest.NewRequest(http.MethodGet, "/expenses/1", nil)
+	req.Header.Set("Content-Type", "application/json")
+	rec := httptest.NewRecorder()
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Errorf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	mock.ExpectQuery("SELECT (.+) FROM expenses").
+		WithArgs("1").
+		WillReturnRows(sqlmock.NewRows([]string{"id", "title", "amount", "note", "tags"}).
+			AddRow(1, "strawberry smoothie", 79, "night market promotion discount 10 bath", pq.Array(&[]string{"food", "beverage"})))
+	h := NewHandler(db)
+	r := gin.Default()
+	r.GET("/expenses/:id", h.Get)
+	expect := "{\"id\":1,\"title\":\"strawberry smoothie\",\"amount\":79,\"note\":\"night market promotion discount 10 bath\",\"tags\":[\"food\",\"beverage\"]}"
 
 	// Act
+	r.ServeHTTP(rec, req)
 
 	// Assert
-	t.Error("not implemented yet")
+	assert.NoError(t, mock.ExpectationsWereMet())
+	assert.Equal(t, http.StatusOK, rec.Code)
+	assert.Equal(t, expect, strings.TrimSpace(rec.Body.String()))
 }
