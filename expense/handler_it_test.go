461013b616a9ac2e92a8ed2bd1909232a9a56a63
jeerapat <jeerapat@mfec.co.th>
2023-01-03 13:56:39 +0700

    create IT test for test get expense detail by id

--- expense/handler_it_test.go
+++ expense/handler_it_test.go
@@ -30,6 +30,9 @@ func setupIT(t *testing.T) func() {
 	}
 	h := NewHandler(db)
 	r.POST("/expenses", h.Create)
+	r.GET("/expenses/:id", h.Get)
+	r.GET("/expenses", h.GetAll)
+	r.PUT("/expenses/:id", h.Update)
 
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%d", serverPort),
@@ -99,5 +102,57 @@ func TestITCreateExpense(t *testing.T) {
 	if assert.NoError(t, err) {
 		assert.Equal(t, http.StatusCreated, resp.StatusCode)
 		assert.Equal(t, byteExpect, byteBody)
+	}
+}
+
+func TestITGetExpenseById(t *testing.T) {
+	// Setup server
+	teardown := setupIT(t)
+	defer teardown()
+
+	// Arrange
+	reqCreateBody := `{
+		"title": "strawberry smoothie",
+		"amount": 79,
+		"note": "night market promotion discount 10 bath", 
+		"tags": ["food", "beverage"]
+	}`
+	reqCreate, err := http.NewRequest(http.MethodPost, fmt.Sprintf("http://localhost:%d/expenses", serverPort), strings.NewReader(reqCreateBody))
+	assert.NoError(t, err)
+	reqCreate.Header.Set("Content-Type", "application/json")
+
+	client := http.Client{}
+
+	// Act
+	respCreate, err := client.Do(reqCreate)
+	assert.NoError(t, err)
+	byteCreateBody, err := ioutil.ReadAll(respCreate.Body)
+	assert.NoError(t, err)
+	respCreate.Body.Close()
+
+	var createdExpense Expense
+	err = json.Unmarshal(byteCreateBody, &createdExpense)
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusCreated, respCreate.StatusCode)
+	}
+
+	reqGetBody := ``
+	reqGet, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://localhost:%d/expenses/%d", serverPort, createdExpense.ID), strings.NewReader(reqGetBody))
+	assert.NoError(t, err)
+	reqGet.Header.Set("Content-Type", "application/json")
+
+	respGet, err := client.Do(reqGet)
+	assert.NoError(t, err)
+	byteGetBody, err := ioutil.ReadAll(respGet.Body)
+	assert.NoError(t, err)
+	respCreate.Body.Close()
+
+	// Assertion
+	expect := createdExpense
+	byteExpect, err := json.Marshal(expect)
+
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusOK, respGet.StatusCode)
+		assert.Equal(t, byteExpect, byteGetBody)
 	}
 }
